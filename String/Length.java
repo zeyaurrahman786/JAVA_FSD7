86f79d929e06df7fc1e8bfa5481637087b10ba26
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-03-30 00:28:21 +0530

    changed

---
+++ String/Length.java
@@ -0,0 +1,13 @@
+public class Length {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(5 ));
+        String str1 = "I am a Student";
+        // System.out.println(str1.length());
+
+
+        String str2 = "I am Here, to Study but failed because of Abhi.";
+        System.out.println(str2.);
+    }
+}
