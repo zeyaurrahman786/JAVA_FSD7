1da4dbbadb555ef4dfc55f94d4b018ea4235737a
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-04-06 02:03:31 +0530

    Threads

---
+++ String/MultiThreading.java
@@ -0,0 +1,20 @@
+public class Multithreading extends Thread{
+    public void run() {
+        System.out.println();
+    }
+    for(int i=1, i<5, i++){
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public static void main(String[] args) {
+        CreateThread t1 = new CreateThread();
+        CreateThread t2 = new CreateThread();
+
+        t1.start();
+        t2.start();
+    }
+}
